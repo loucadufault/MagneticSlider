e62a7af9297a0952f98aca6dadc49cb8a08e53d6
loucadufault <40028187+loucadufault@users.noreply.github.com>
2021-09-09 14:23:41 -0400

    documentation, include utils, anonymize export

--- MagneticSlider.js
+++ MagneticSlider.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Slider from "@material-ui/core/Slider";
 
-// TODO move to utils
+
+const DEFAULT_MAGNETIC_RADIUS = 100;
+const DEFAULT_MAGNETIC_BOUNDARY_DEADZONE = 0;
+
+const DEFAULT_DEFAULT_VALUE = 0; // we follow material-ui's convention of falling back to 0 if no value or default value is given
+
+
 // @see https://stackoverflow.com/a/53187807
-export function findLastIndex(array, predicate) {
-  // should leverage the fact that the input array is sorted for perf
+function findLastIndex(array, predicate) {
   let l = array.length;
   while (l--) {
     if (predicate(array[l], l, array)) return l;
@@ -12,48 +17,19 @@ export function findLastIndex(array, predicate) {
   return -1;
 }
 
-const DEFAULT_MAGNETIC_RADIUS = 100,
-  DEFAULT_MAGNETIC_BOUNDARY_DEADZONE = 0;
 
-// pre-process the marks once from the props to sort by value (ASC) and to filter out the non-magnetic marks
-function withSortedMarks(WrappedSliderComponent) {
-  // HoC
-  return function(props) {
-    const sortedMarks = props.hasOwnProperty("marks")
-      ? [...props.marks].sort((a, b) => a.value - b.value)
-      : [];
-    const cleanedMarks = props.hasOwnProperty("marks")
-      ? [...props.marks].map((mark) => {
-          const clone = Object.assign({}, mark);
-          delete clone.magneticScale;
-        })
-      : [];
-
-    // also, ensure that the statefulness is passed down to the wrapped component, creating it at this level if needed
-    let value, setValue;
-    if (!props.hasOwnProperty("value")) {
-      const state = React.useState(props.defaultValue);
-      [value, setValue] = state;
-    }
-
-    return (
-      <MagneticSlider
-        sortedMarks={sortedMarks}
-        marks={cleanedMarks}
-        value={props.hasOwnProperty("value") ? props.value : value}
-        setValue={props.hasOwnProperty("value") ? props.setValue : setValue} // should console.warn if props.setValue is undefined (or find a better mechanism to pass down state that does not involve setting both)
-        {...props}
-      />
-    );
-  };
+function findSortedLastIndex(array, predicate) {
+  return findLastIndex(array, predicate); // TODO leverage the fact that the input array is sorted for perf
 }
+
 
 function MagneticSlider(props) {
   const {
     sortedMarks,
-    setValue,
+    value,
     magneticRadius = DEFAULT_MAGNETIC_RADIUS,
     magneticBoundaryDeadzone = DEFAULT_MAGNETIC_BOUNDARY_DEADZONE,
+    onChange: parentOnChange,
     ...otherProps
   } = props;
 
@@ -61,11 +37,13 @@ function MagneticSlider(props) {
     if (lowNeighbourMark === undefined && highNeighbourMark === undefined) {
       return null;
     } else if (lowNeighbourMark === undefined) {
-      return Math.abs(highNeighbourMark.value - value) < magneticRadius * lowNeighbourMark.magneticScale
+      return Math.abs(highNeighbourMark.value - value) <
+        magneticRadius * highNeighbourMark.magneticScale
         ? highNeighbourMark.value
         : null;
     } else if (highNeighbourMark === undefined) {
-      return Math.abs(value - lowNeighbourMark.value) < magneticRadius * highNeighbourMark.magneticScale
+      return Math.abs(value - lowNeighbourMark.value) <
+        magneticRadius * lowNeighbourMark.magneticScale
         ? lowNeighbourMark.value
         : null;
     } else {
@@ -89,7 +67,6 @@ function MagneticSlider(props) {
           return lowNeighbourMark.value;
         }
       } else {
-        // >=
         if (highDistance < magneticRadius * highNeighbourMark.magneticScale) {
           return highNeighbourMark.value;
         }
@@ -101,17 +78,16 @@ function MagneticSlider(props) {
 
   return (
     <Slider
-      value={props.value}
+      value={value}
       onChange={(event, newValue) => {
-        const lesserMarkIndex = findLastIndex(
+        const lesserMarkIndex = findSortedLastIndex(
           sortedMarks.map((mark) => mark.value),
           (value) => value <= newValue
         );
 
         let magnetTarget;
-        // 3 cases: 1) it has a lesser and greater mark index, 2) it just has a greater mark index, 3) it has just a lesser mark index
         if (lesserMarkIndex === -1) {
-          // newValue is either 
+          // newValue is either
           magnetTarget = computeMagneticTarget(
             newValue,
             undefined,
@@ -133,15 +109,10 @@ function MagneticSlider(props) {
 
         if (magnetTarget === null) {
           // no-op
-          setValue(newValue);
+          parentOnChange(event, newValue);
         } else {
           // magnet to nearest mark
-          setValue(magnetTarget);
-        }
-
-        // run parent hook
-        if (props.hasOwnProperty("onChange")) {
-          props.onChange(event, newValue); // make sure to respect API signature
+          parentOnChange(event, magnetTarget);
         }
       }}
       {...otherProps}
@@ -149,6 +120,80 @@ function MagneticSlider(props) {
   );
 }
 
-const OptimizedMagneticSlider = withSortedMarks(MagneticSlider);
 
-export default OptimizedMagneticSlider;
+/**
+ * 
+ * @param {*} WrappedSliderComponent the MagneticSlider component that this HoC wraps
+ * 
+ * Pre-processes the props marks once to sort by (ASC) value.
+ * Cleans the props to comply to the API of the underlying Slider (namely, the marks array element objects may contain non-compliant properties provided for our MagneticSlider component).
+ *
+ * The MagneticSlider component requires a given value prop, that it can pass down to its underlying Slider component to manage its value. 
+ * This value is either provided from above (parent of this HoC), or created as a state in this HoC. In any case the value is always provided downstream to the wrapped MagneticSlider component.
+ * In the former case, we cannot directly manage the statefulness of the passed value (it may not even be stateful), but can only notify the parent of the target value via the onChange handler prop if one is provided by said parent. This conforms with the observed behavior of a controlled Slider component.
+ * In the latter case, we create and manage the statfulness of the value, and pass down this stateful value as well as an onChange handler that controls its state to the wrapped MagneticSlider. This conforms with the observed behavior of an uncontrolled Slider component.
+ */
+function withSortedMarks(WrappedMagneticSliderComponent) {
+  return function(props) {
+    const {
+      marks,
+      value: parentValue,
+      onChange: parentOnChange,
+      defaultValue = DEFAULT_DEFAULT_VALUE,
+      ...otherProps
+    } = props;
+
+    const sortedMarks = props.hasOwnProperty("marks")
+      ? [...marks].sort((a, b) => a.value - b.value)
+      : [];
+
+    const cleanedMarks = props.hasOwnProperty("marks")
+      ? [...marks].map((mark) => {
+          const clone = Object.assign({}, mark);
+          delete clone.magneticScale;
+          return clone;
+        })
+      : [];
+
+    // ensure that the statefulness is passed down to the wrapped component, creating it at this level if needed
+    let value, onChange;
+    // there is no statefulness from above (not controlled)
+    if (!props.hasOwnProperty("value")) {
+      let setValue; // create and manage own state
+      [value, setValue] = React.useState(defaultValue);
+
+      onChange = (event, newValue) => {
+        setValue(newValue); // the wrapped component will update the statefulness with this hook
+
+        // call the parent hook, if any
+        if (props.hasOwnProperty("onChange")) {
+          parentOnChange(event, newValue);
+          // note that the parent hook cannot access the statefulness in this case
+        }
+      };
+    }
+
+    let onChangeCb;
+    // there is statefulness from above (controlled)
+    if (props.hasOwnProperty("value")) {
+      if (props.hasOwnProperty("onChange")) {
+        onChangeCb = parentOnChange; // let the parent exclusively handle this (provided there is a parent handler). we do not handle own state
+      } // else undefined
+    } else {
+      onChangeCb = onChange; // otherwise we handle this and update own state
+    }
+
+    return (
+      <WrappedMagneticSliderComponent
+        sortedMarks={sortedMarks}
+        marks={cleanedMarks}
+        value={props.hasOwnProperty("value") ? parentValue : value}
+        {...(onChangeCb !== undefined ? { onChange: onChangeCb } : {})} // condtionally add this prop
+        {...otherProps}
+      />
+    );
+  };
+}
+
+
+export default withSortedMarks(MagneticSlider);
